d453977913f8258461421b9fbea25782b10e1f3b
Alexander <51708858+Web-developer-Alexander@users.noreply.github.com>
2022-10-19 12:10:13 +0300

    The desktop version is ready
    The desktop version is fully ready, added pages about us, gallery, blog, article, contacts

--- app/js/main.js
+++ app/js/main.js
@@ -14,6 +14,18 @@ $(function () {
         autoplay: true
     });
 
+    $('.contact-slider').slick({
+        slidesToShow: 10,
+        slidesToScroll: 10,
+        dots: true,
+        arrows: false,
+    });
+
+    $('.article-slider__box').slick({
+        prevArrow: '<button type="button" class="article-slider__arrow article-slider__arrowleft"><img src="images/arrow-slide-left.svg" alt="arrow left"></button>',
+        nextArrow: '<button type="button" class="article-slider__arrow article-slider__arrowright"><img src="images/arrow-slide-right.svg" alt="arrow right"></button>'
+    });
+
     var mixer = mixitup('.gallery__inner', {
         load: {
             filter: '.living'
