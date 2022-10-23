ed474093c534bb4bce59862a6c79fff64132ffea
Alexander <51708858+Web-developer-Alexander@users.noreply.github.com>
2022-10-23 20:02:58 +0300

    Responsive Design & Bug Fixes
    Adaptive design, bug fixes, compression of project files

--- app/js/main.js
+++ app/js/main.js
@@ -7,6 +7,15 @@ $(function () {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
 
+
+    $('.header__btn-menu').on('click', function () {
+        $('.menu').toggleClass('menu--open');
+    });
+
+    if ($(window).width() <= 650) {
+        $('.works-path__item--measurements').appendTo($('.works-path__items-box'));
+    }
+
     $('.top__slider').slick({
         dots: true,
         arrows: false,
@@ -19,6 +28,50 @@ $(function () {
         slidesToScroll: 10,
         dots: true,
         arrows: false,
+        responsive: [
+            {
+                breakpoint: 1700,
+                settings: {
+                    slidesToShow: 8,
+                    slidesToScroll: 8,
+                }
+            },
+            {
+                breakpoint: 1511,
+                settings: {
+                    slidesToShow: 6,
+                    slidesToScroll: 6
+                }
+            },
+            {
+                breakpoint: 1201,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4
+                }
+            },
+            {
+                breakpoint: 841,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 651,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 415,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 
     $('.article-slider__box').slick({
